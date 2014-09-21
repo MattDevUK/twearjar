57d40087a34914e26bc2d95a24f1a7d242a18d2a
Matthew <matthinesdev+heroku@gmail.com>
2014-09-21 12:09:55 +0100

    Updated db reset

--- reset_db.js
+++ reset_db.js
@@ -1,5 +1,6 @@
 use twearjar;
-db.usercollection.update({"username": "@twearjar"}, {$set:{"swearCount":0, "totalSwearCount":0}});
+db.usercollection.remove({username:"@twearjar"});
 db.usercollection.remove({username:"@matthdevuk"});
 db.usercollection.remove({username:"@matthdev"});
 db.usercollection.remove({username:"@mattdevuk"});
+db.usercollection.insert({"user_id": "2821769120", "username": "@twearjar", "swearCount": 0, "totalSwearCount":0});
